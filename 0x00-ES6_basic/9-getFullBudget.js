dc3aa809ac77186d288e63687358ebb5ceba8ed3
wendy munyasi <wendymunyasi@gmail.com>
2022-12-19 23:58:32 +0300

    Add task 9 - use ES6 method properties

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,16 @@
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars: function (income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros: function (income) {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
