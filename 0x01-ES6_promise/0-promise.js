2a8dd58becfb980a5fb846feabbe9ada59f94280
wendy munyasi <wendymunyasi@gmail.com>
2022-12-20 11:26:26 +0300

    Add task 0 - return a promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // return new Promise(function(resolve, reject) {} works too
+    // fetch something e.g data and make a request
+    // if it's a success call the resolve function and pass the data
+    // if there's an error call reject and pass the error through
+  });
+}
