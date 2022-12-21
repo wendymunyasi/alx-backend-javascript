6139cd8a3774681eac2f5c71c589f9907befbf69
wendy munyasi <wendymunyasi@gmail.com>
2022-12-21 11:02:47 +0300

    Attempt to fix codefails - task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(response => {
-    console.log('Got a response from the API');
-    return {
-      status: 200,
-      body: 'success',
-    };
-  }).catch(error => new Error());
+  return promise
+    .then(response => {
+      console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch(() => new Error());
 }
