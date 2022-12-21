170307d501d00da09d024fbc53206c50bb2091c1
wendy munyasi <wendymunyasi@gmail.com>
2022-12-21 12:16:03 +0300

    Attempt to fix codefails - task 3 - try 2

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,17 +3,13 @@ import { createUser, uploadPhoto } from './utils';
 export default function handleProfileSignup() {
   // Call the uploadPhoto function to upload the user's photo then chain all
   // promises together and handle with one error statement
-  uploadPhoto()
-    .then((photoResp) => createUser({
-      // the details below are provided in the createUser function
-      // fetch them using dot notation
-      body: photoResp.body,
-      firstName: photoResp.firstName,
-      lastName: photoResp.lastName,
-    })).then((userResp) => {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((userResp) => {
       // If the user was successfully created, log the body, firstName, and lastName to the console
-      console.log(userResp.body, userResp.firstName, userResp.lastName);
-    }).catch(() => {
+      console.log(`${userResp[0].body} ${userResp[1].firstName} ${userResp[1].lastName}`);
+    })
+    .catch(() => {
       // In the event of an error, log Signup system offline to the console
       console.log('Signup system offline');
     });
