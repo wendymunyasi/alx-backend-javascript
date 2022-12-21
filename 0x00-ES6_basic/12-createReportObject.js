964d6cb3a56ae9ca9dc9df62fec609cbbb91e6f0
wendy munyasi <wendymunyasi@gmail.com>
2022-12-21 13:00:02 +0300

    Fix codefails - task 12

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,22 +1,12 @@
 export default function createReportObject(employeesList) {
-  // Create an empty object to store the employees by department
-  const allEmployees = {};
-
-  // Iterate over the employeesList array
-  for (const employee of employeesList) {
-    // Destructure the department and name properties from the employee object
-    const { department, name } = employee;
-    if (!allEmployees[department]) {
-      allEmployees[department] = [];
-    }
-    allEmployees[department].push(name);
-  }
-  // Return an object with the allEmployees object and a method to get the number of departments
+  // Return an object with the allEmployees object and a method to get th
+  // number of departments
   return {
-    allEmployees,
-    getNumberOfDepartments() {
-      // Return no of keys in the allEmployees object (which represents the number of departments)
-      return Object.keys(this.allEmployees).length;
+    allEmployees: employeesList,
+    getNumberOfDepartments(employeesList) {
+      // Return no of keys in the allEmployees object (which represents the
+      // number of departments)
+      return Object.keys(employeesList).length;
     },
   };
 }
