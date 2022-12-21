192421eb1badbe2710dd849507aa4fa770293d76
wendy munyasi <wendymunyasi@gmail.com>
2022-12-21 11:28:17 +0300

    Fix codefails - task 4 - try 1

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,5 +1,5 @@
 export default function signUpUser(firstName, lastName) {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     resolve({
       firstName,
       lastName,
