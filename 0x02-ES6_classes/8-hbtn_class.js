735c816c2f39754f8d0734b208df757b4db0124e
wendy munyasi <wendymunyasi@gmail.com>
2022-12-22 23:45:54 +0300

    Add task 8 - implement a class HolbertonClass

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-underscore-dangle */
+export default class HolbertonClass {
+  /**
+   * @param {number} size - no of students in class
+   * @param {string} location - location of class
+   */
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    this._size = value;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(value) {
+    this._location = value;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this.size;
+    }
+    if (hint === 'string') {
+      return this.location;
+    }
+    return this;
+  }
+}
