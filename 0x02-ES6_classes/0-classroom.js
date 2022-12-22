9b70576916b881b7e2b49bb4f65e05bcc2850bff
wendy munyasi <wendymunyasi@gmail.com>
2022-12-22 08:35:07 +0300

    Add task 0 - implement a class ClassRoom

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-underscore-dangle */
+export default class ClassRoom {
+  /**
+   * Implements a class named ClassRoom
+   * @param {Number} maxStudentsSize - maximum number of students in class
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
