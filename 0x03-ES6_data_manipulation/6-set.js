b91436fe0b7f73690ee43e7641c638435abb5324
wendy munyasi <wendymunyasi@gmail.com>
2023-01-03 14:46:48 +0300

    Add task 6 - func that returns set from array

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a new Set from the elements in the given array.
+ *
+ * @param {Array} array - The array to create the Set from
+ * @return {Set} A new Set containing the elements from the array
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
