89e6e6c376389ba376825af5cd4b6bb70bf16cf3
wendy munyasi <wendymunyasi@gmail.com>
2023-02-21 23:07:54 +0300

    Add task 0 - function named displayMessage

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Prints the given message to STDOUT.
+ *
+ * @param {msg} str - The message to be printed.
+ */
+function displayMessage(msg) {
+  console.log(msg);
+}
+
+module.exports = displayMessage;
