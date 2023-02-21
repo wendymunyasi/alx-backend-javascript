b1efacbb6f4054704970d91adbfa28b0de1df591
wendy munyasi <wendymunyasi@gmail.com>
2023-02-21 23:31:58 +0300

    Fix eslint errors

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -22,5 +22,5 @@ process.stdin.on('readable', () => {
 });
 
 process.stdin.on('end', () => {
-    process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
